0d221cfebf4f0a7ad2c091b87df9effa824d749a
sdhaduk <sagardhaduk1@gmail.com>
2023-12-29 17:32:39 -0600

    Created sign-up and sign-in forms

--- src/components/form/SignUpForm.tsx
+++ src/components/form/SignUpForm.tsx
@@ -17,15 +17,23 @@ import Link from "next/link";
 
 type Props = {};
 
-const FormSchema = z.object({
-  email: z.string().min(1, "Email is required").email("Invalid Email"),
-  password: z
-    .string()
-    .min(1, "Password is required")
-    .min(8, "Password must have more than 8 characters"),
-});
+const FormSchema = z
+  .object({
+    username: z.string().min(1, "Username is required").max(30),
+    email: z.string().min(1, "Email is required").email("Invalid Email"),
+    password: z
+      .string()
+      .min(1, "Password is required")
+      .min(8, "Password must have more than 8 characters"),
 
-const SignInForm = (props: Props) => {
+    confirmPassword: z.string().min(1, "Password confirmation is required"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Passwords do not match",
+  });
+
+const SignUpForm = (props: Props) => {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
   });
@@ -38,6 +46,19 @@ const SignInForm = (props: Props) => {
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
         <div className="space-y-2">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="John Doe" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <FormField
             control={form.control}
             name="email"
@@ -68,9 +89,26 @@ const SignInForm = (props: Props) => {
               </FormItem>
             )}
           />
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirm Password</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Re-enter password"
+                    type="password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </div>
         <Button type="submit" className="w-full mt-6">
-          Sign In
+          Sign Up
         </Button>
       </form>
       <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
@@ -78,12 +116,12 @@ const SignInForm = (props: Props) => {
       </div>
       <p className="text-center text-sm text-gray-600 mt-2">
         If you don&apos;t have an account, please&nbsp;
-        <Link className="text-blue-500 hover:underline" href="/sign-up">
-          Sign up
+        <Link className="text-blue-500 hover:underline" href="/sign-in">
+          Sign In
         </Link>
       </p>
     </Form>
   );
 };
 
-export default SignInForm;
+export default SignUpForm;
