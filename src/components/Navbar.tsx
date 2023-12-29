cbdf68e4b14aecb5f9d7278bd7ec170f7787d31e
sdhaduk <sagardhaduk1@gmail.com>
2023-12-29 16:45:47 -0600

    created navbar and added shadcn button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { buttonVariants } from "./ui/button";
+import { HandMetal } from "lucide-react";
 
-type Props = {}
+type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <div className='bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0'>
-        <div>
-            Login
-        </div>
-
+    <div className="bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
+      <div className="container flex items-center justify-between">
+        <Link href="/">
+          <HandMetal />
+        </Link>
+        <Link className={buttonVariants()} href="/sign-in">
+          Sign in
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
