98b5755207f09b246865810335d32888711fff5b
sdhaduk <sagardhaduk1@gmail.com>
2023-12-29 16:18:23 -0600

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <div className='bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0'>
+        <div>
+            Login
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
