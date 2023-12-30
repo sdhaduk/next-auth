3f5eeb2b6b6bd68bcc76bb2d41d545688e35ae8f
sdhaduk <sagardhaduk1@gmail.com>
2023-12-30 02:29:08 -0600

    Added username to admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,19 @@
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+
 type Props = {};
 
-const page = (props: Props) => {
-  return <div>Welcome to admin</div>;
+const page = async (props: Props) => {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    return (
+      <h2 className="text-2xl">
+        Admin Page - Welcome Back {session?.user.username}
+      </h2>
+    );
+  }
+
+  return <h2 className="text-2xl">Login to see the Admin Page </h2>;
 };
 
 export default page;
