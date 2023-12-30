977ade30e89301525a05c2175e1fb90d9a83bbe8
sdhaduk <sagardhaduk1@gmail.com>
2023-12-30 01:25:26 -0600

    add log-in functionality

--- src/components/form/SignInForm.tsx
+++ src/components/form/SignInForm.tsx
@@ -14,8 +14,10 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import Link from "next/link";
-import GoogleSignInButton from "../GithubSignInButton";
+import GithubSignInButton from "../GithubSignInButton";
 import { Github } from "lucide-react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 type Props = {};
 
@@ -28,12 +30,23 @@ const FormSchema = z.object({
 });
 
 const SignInForm = (props: Props) => {
+  const router = useRouter();
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
   });
 
-  const onSubmit = (values: z.infer<typeof FormSchema>) => {
-    console.log(values);
+  const onSubmit = async (values: z.infer<typeof FormSchema>) => {
+    const signInData = await signIn("credentials", {
+      email: values.email,
+      password: values.password,
+      redirect: false,
+    });
+
+    if (signInData?.error) {
+      console.log(signInData.error);
+    } else {
+      router.push("/admin");
+    }
   };
 
   return (
@@ -78,7 +91,9 @@ const SignInForm = (props: Props) => {
       <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
         or
       </div>
-      <GoogleSignInButton>Sign In with&nbsp; <Github /></GoogleSignInButton>
+      <GithubSignInButton>
+        Sign In with&nbsp; <Github />
+      </GithubSignInButton>
       <p className="text-center text-sm text-gray-600 mt-2">
         If you don&apos;t have an account, please&nbsp;
         <Link className="text-blue-500 hover:underline" href="/sign-up">
