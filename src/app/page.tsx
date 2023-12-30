37f1a913cbaf05a12cd5ce595cc6d65a628a5cd0
sdhaduk <sagardhaduk1@gmail.com>
2023-12-30 01:24:39 -0600

    created admin page and added button to redirect to it from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import { buttonVariants } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <h1 className="text-4xl">Home</h1>;
+  return (
+    <div>
+      <Link className={buttonVariants()} href="/admin">
+        Open My Admin
+      </Link>
+    </div>
+  );
 }
+ 
